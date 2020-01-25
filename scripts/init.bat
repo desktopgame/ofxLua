76e4de50fc70c9e3eb45f933616940b47c6f9afe
desktopgame <koya0306@outlook.jp>
2020-01-25 23:28:39 +0900

    特定のバージョンをダウンロードするように

--- scripts/init.bat
+++ scripts/init.bat
@@ -1,3 +1,3 @@
 curl -O https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-nuget install lua -OutputDirectory ../libs/lua/packages
+nuget install lua -OutputDirectory ../libs/lua/packages -Version 5.3.5.1
 del nuget.exe
