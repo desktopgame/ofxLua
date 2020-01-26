6838ceb4f3e2c3a469b0ea457f5c46da7718b22f
desktopgame <koya0306@outlook.jp>
2020-01-26 13:39:33 +0900

    コメントを追加

--- src/ofxLua.h
+++ src/ofxLua.h
@@ -60,30 +60,102 @@ public:
 	explicit Interpreter();
 	~Interpreter();
 
+	/**
+	 * load a file as lua script.
+	 * @param file
+	 * @return
+	 */
 	TResult<Unit, ErrorCode> loadFile(const std::string& file);
 
+	/**
+	 * load a string as lua script.
+	 * @param str
+	 * @return
+	 */
 	TResult<Unit, ErrorCode> loadString(const std::string& str);
 
+	/**
+	 * call lua function.
+	 * @param function
+	 * @param args
+	 * @param returnType
+	 * @return
+	 */
 	TResult<std::vector<Variant>, Unit> call(const std::string& function, std::vector<Object> args, std::vector<Type> returnType);
 
+	/**
+	 * define function into lua script.
+	 * @param name
+	 * @param fn
+	 * @return
+	 */
 	Result define(const std::string& name, Function fn);
 
+	/**
+	 * define specified value into lua script.
+	 * @param name
+	 * @param value
+	 * @return
+	 */
 	Result setGlobalNumber(const std::string& name, double value);
 
+	/**
+	 * define specified value into lua script.
+	 * @param name
+	 * @param value
+	 * @return
+	 */
 	Result setGlobalInt(const std::string& name, int value);
 
+	/**
+	 * define specified value into lua script.
+	 * @param name
+	 * @param value
+	 * @return
+	 */
 	Result setGlobalBool(const std::string& name, bool value);
 
+	/**
+	 * define specified value into lua script.
+	 * @param name
+	 * @param value
+	 * @return
+	 */
 	Result setGlobalString(const std::string& name, const std::string& value);
 
+	/**
+	 * load global variable from lua script.
+	 * @param name
+	 * @return
+	 */
 	TResult<double, Unit> getGlobalNumber(const std::string& name);
 
+	/**
+	 * load global variable from lua script.
+	 * @param name
+	 * @return
+	 */
 	TResult<int, Unit> getGlobalInt(const std::string& name);
 
+	/**
+	 * load global variable from lua script.
+	 * @param name
+	 * @return
+	 */
 	TResult<bool, Unit> getGlobalBool(const std::string& name);
 
+	/**
+	 * load global variable from lua script.
+	 * @param name
+	 * @return
+	 */
 	TResult<std::string, Unit> getGlobalString(const std::string& name);
 
+	/**
+	 * load all global variable from lua script.
+	 * @param name
+	 * @return
+	 */
 	TResult<std::unordered_map<std::string, Object>, Unit> getAllVariables() const;
 
 	bool isOccurredError() const;
